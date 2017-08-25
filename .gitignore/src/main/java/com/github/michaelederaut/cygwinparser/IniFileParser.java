3957099bbc010b9e81009d3ee041109c0ffed1e0
MichaelEderAut <Michael.Eder.Vie@gmx.at>
2017-08-25 03:10:09 +0200

    Add files via upload

---
+++ .gitignore/src/main/java/com/github/michaelederaut/cygwinparser/IniFileParser.java
@@ -0,0 +1,442 @@
+package com.github.michaelederaut.cygwinparser;
+
+import static com.github.michaelederaut.basics.RegexpUtils.GroupMatchResult;
+import static com.github.michaelederaut.basics.RegexpUtils.NamedPattern;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.mutable.*;
+
+import com.github.michaelederaut.basics.EnumReflectionUtils;
+import com.github.michaelederaut.basics.LineNbrRandomAccessFile;
+import com.github.michaelederaut.basics.RegexpUtils;
+import com.github.michaelederaut.basics.LineNbrRandomAccessFile.ReadLinePolicy;
+import com.github.michaelederaut.cygwinparser.SetupIniContents.ArchInfo;
+import com.github.michaelederaut.cygwinparser.SetupIniContents.PckgInfo;
+
+import regexodus.Pattern;
+
+public class IniFileParser {
+	
+private static final String PKG_NAME          = "pkg_name";
+private static final String DESCRIPTION_SHORT = "sdesc";
+private static final String DESCRIPTION_LONG  = "ldesc";
+private static final String CLOSING_QUOTE     = "closing_quote";
+private static final String CATEGORY          = "category";
+private static final String REQUIRES          = "requires";
+private static final String VERSION           = "version";
+private static final String INSTALL           = "install";
+private static final String SOURCE            = "source";
+
+private static final String S_re_comment_line = "^\\#.*$";
+private static final NamedPattern P_comment_line =  NamedPattern.FO_compile(S_re_comment_line);
+
+private static final String S_re_pkg_hdr = "^@\\s+({" + PKG_NAME + "}[A-Za-z0-9._+\\-]+)\\s*$";
+private static final NamedPattern P_pkg_hdr = NamedPattern.FO_compile(S_re_pkg_hdr);
+
+private static final String S_re_sdesc = "^" + DESCRIPTION_SHORT + "\\:\\s+\"({" + DESCRIPTION_SHORT + "}[^\"]*?)\"$";
+private static final NamedPattern P_sdesc =  NamedPattern.FO_compile(S_re_sdesc);
+
+private static final String S_re_ldesc = "^" + DESCRIPTION_LONG + "\\:\\s+\"({" + DESCRIPTION_LONG +   "}[^\"]*?)({" + CLOSING_QUOTE + "}\")?$";
+private static final NamedPattern P_ldesc =  NamedPattern.FO_compile(S_re_ldesc);
+
+private static final String S_re_ldesc_cont    = "^({" + DESCRIPTION_LONG + "}[^\"]*?)({" + CLOSING_QUOTE + "}\")?$";
+private static final NamedPattern P_ldesc_cont = NamedPattern.FO_compile(S_re_ldesc_cont);
+
+private static final String S_re_catgegory = "^" + CATEGORY + "\\:\\s+({" + CATEGORY + "}[a-zA-Z0-9._+\\-]+)\\s*$";
+static final NamedPattern   P_category     =  NamedPattern.FO_compile(S_re_catgegory);
+
+private static final String S_re_requires = "^" + REQUIRES + "\\:\\s+({" + REQUIRES + "}[^\"]*?)$";
+private static final NamedPattern P_requires =  NamedPattern.FO_compile(S_re_requires);
+
+private static final String S_re_version = "^" + VERSION + "\\:\\s+({" + VERSION + "}[a-zA-Z0-9._+\\-]+)\\s*$";
+static final NamedPattern   P_version    =  NamedPattern.FO_compile(S_re_version);
+
+private static final String S_re_archinfo = "^([a-zA-Z0-9._+\\-/]+)\\s+(\\d+)\\s+(\\p{XDigit}{128})$";
+static final NamedPattern   P_archinfo    =  NamedPattern.FO_compile(S_re_archinfo);
+
+// private static final String S_re_install = "^" + INSTALL + "\\:\\s+({" + INSTALL + "}[^\"\\s]*?)$";
+private static final String S_re_install = "^" + INSTALL + "\\:\\s+({" + INSTALL + "}[^\"]*?)$";
+private static final NamedPattern P_install =  NamedPattern.FO_compile(S_re_install);
+
+private static final String S_re_source = "^" + SOURCE + "\\:\\s+({" + SOURCE + "}[^\"]*?)$";
+private static final NamedPattern P_source =  NamedPattern.FO_compile(S_re_source);
+
+private static final String PREV_MARKER = "[prev]";
+
+// private enum ParsingState {Init, PkgName, Sdesc, Ldesc, LdescCont, Category, Requires, InterPgk, Finished};
+
+private enum ParsingState {Init, PkgName, Sdesc, Ldesc, LdescCont, Category, Requires, 
+	Version,     Install,    Source,     Prev,
+	VersionPrev, InstallPrev, SourcePrev,
+	InterPgk, Finished;
+};
+
+private final static int  I_parsing_state_Source        = ParsingState.Source.ordinal();
+// private final static int  I_parsing_state_VersionPrev   = ParsingState.VersionPrev.ordinal();
+private final static int  I_parsing_state_finished      = ParsingState.Finished.ordinal();
+private final static int I_prev_offset = ParsingState.VersionPrev.ordinal() - ParsingState.Version.ordinal();
+
+private static class MutableParsingState  extends MutableObject<ParsingState> {
+	
+	public ParsingState E_parsing_state;
+	public int          I_offset;
+	
+	public MutableParsingState(final ParsingState PI_E_parsing_state) {
+		this.E_parsing_state = PI_E_parsing_state;
+		return;
+	}
+	
+	public MutableParsingState(
+			final ParsingState PI_E_parsing_state,
+			final int          PI_I_offset) {
+		this.E_parsing_state = PI_E_parsing_state;
+		this.I_offset        = PI_I_offset;
+	}
+	
+	@Override
+	public ParsingState getValue() {
+		return this.E_parsing_state;
+	}
+	
+	
+	@Override
+	public void setValue(final ParsingState PI_E_parsing_state) {
+		this.E_parsing_state = PI_E_parsing_state; 
+	}
+}
+
+private static SetupIniContents.PckgVersionInfo FO_parse_pck_info (
+		final LineNbrRandomAccessFile    PB_O_buff_reader,
+		final MutableParsingState PB_OM_parsing_state) {
+	
+	RuntimeException                 E_rt;
+	AssertionError                   E_assert;
+//	AssertionError                   E_assert;
+
+	ParsingState                     E_parsing_state;
+	GroupMatchResult                 O_grp_match_res;
+	SetupIniContents.ArchInfo        O_install, O_src;
+	SetupIniContents.PckgVersionInfo O_retval_pck_vers_info;
+	String                           S_msg_1, S_msg_2, S_line_input, 
+	                                 S_version, S_pn_archive, S_size, S_chk_sum,
+	                                 S_archinfo,  AS_numbered_groups[];
+	int                              I_line_nbr_f1, I_offs_f1;
+	
+	O_retval_pck_vers_info = null;
+	E_parsing_state = PB_OM_parsing_state.getValue();
+
+	S_version = null;
+	O_install = null;
+	O_src     = null;
+	
+	S_line_input = PB_O_buff_reader.FS_re_read_line();
+	I_line_nbr_f1 = PB_O_buff_reader.I_curr_line_nbr;
+	LOOP_INPUT_LINES: while (E_parsing_state.ordinal() < I_parsing_state_Source) {
+		if (S_line_input == null) {
+		   if  ((E_parsing_state == ParsingState.Install) || (E_parsing_state == ParsingState.Source)) {
+			  E_parsing_state = ParsingState.Finished;
+		      }
+		   else {
+			  S_msg_1 = "Unexepectd end of file at line: " +  I_line_nbr_f1 + ".";
+			  E_assert = new AssertionError(S_msg_1);
+			  S_msg_2 = "Unable to instantiate new object of type: \'" + 
+			             SetupIniContents.PckgVersionInfo.class.getName() + "\'";
+			  E_rt = new RuntimeException(S_msg_2, E_assert);
+			  throw E_rt;
+		      }
+		   }
+		else if (E_parsing_state == ParsingState.Requires) {
+			O_grp_match_res = RegexpUtils.FO_match(S_line_input, P_version);
+			if (O_grp_match_res.I_map_size_f1 >= 1) {
+			   S_version = O_grp_match_res.HS_named_groups.get(VERSION).S_val;
+			   E_parsing_state = ParsingState.Version;		
+			   }
+		    }
+		else if (E_parsing_state == ParsingState.Version) {
+			O_grp_match_res = RegexpUtils.FO_match(S_line_input, P_install);
+			if (O_grp_match_res.I_map_size_f1 >= 1) {
+			   S_archinfo = O_grp_match_res.HS_named_groups.get(INSTALL).S_val;
+			   O_grp_match_res = RegexpUtils.FO_match(S_archinfo, P_archinfo);
+			   if (O_grp_match_res.I_array_size_f1 >= 4) {
+				  AS_numbered_groups = O_grp_match_res.AS_numbered_groups;
+				  S_pn_archive = AS_numbered_groups[1];
+				  S_size       = AS_numbered_groups[2];
+				  S_chk_sum    = AS_numbered_groups[3];
+				  O_install = new ArchInfo(S_pn_archive, S_size, S_chk_sum);
+				  E_parsing_state = ParsingState.Install;
+			      }
+			   }
+		    }
+		else if (E_parsing_state == ParsingState.Install) {
+			O_grp_match_res = RegexpUtils.FO_match(S_line_input, P_source);
+			if (O_grp_match_res.I_map_size_f1 >= 1) {
+			   S_archinfo = O_grp_match_res.HS_named_groups.get(SOURCE).S_val;
+			   O_grp_match_res = RegexpUtils.FO_match(S_archinfo, P_archinfo);
+			   if (O_grp_match_res.I_array_size_f1 >= 4) {
+				  AS_numbered_groups = O_grp_match_res.AS_numbered_groups;
+				  S_pn_archive = AS_numbered_groups[1];
+				  S_size       = AS_numbered_groups[2];
+				  S_chk_sum    = AS_numbered_groups[3];
+				  O_src = new ArchInfo(S_pn_archive, S_size, S_chk_sum);
+				  E_parsing_state = ParsingState.Source;
+			      }
+			   }
+			   else if (StringUtils.startsWith(S_line_input, PREV_MARKER)) {
+				   E_parsing_state = ParsingState.Prev;	
+			   }
+		    }
+		try {
+			S_line_input = PB_O_buff_reader.FS_readLine();
+			I_line_nbr_f1 = PB_O_buff_reader.I_curr_line_nbr;
+		} catch (RuntimeException PI_E_rt) {
+		      S_msg_1 = "Error reading input file at line: " + I_line_nbr_f1;
+		      E_rt = new RuntimeException(S_msg_1, PI_E_rt);
+		  throw E_rt;
+		  }
+	}   // END while
+	
+	O_retval_pck_vers_info = new SetupIniContents.PckgVersionInfo(S_version, O_install, O_src);
+	I_offs_f1 =  PB_OM_parsing_state.I_offset;
+	if (I_offs_f1 != 0) {
+	   EnumReflectionUtils.FV_modify_ordinal_by(E_parsing_state, I_offs_f1);
+	   }
+	PB_OM_parsing_state.setValue(E_parsing_state);
+	return O_retval_pck_vers_info;
+}
+
+public static SetupIniContents FO_parse(final LineNbrRandomAccessFile PI_O_buff_reader) {
+		SetupIniContents O_retval_setup_ini_contents;
+		
+		RuntimeException     E_rt;
+		NullPointerException E_np;
+		AssertionError       E_ass;
+		
+		ParsingState E_parsing_state;
+		GroupMatchResult O_grp_match_res;
+		SetupIniContents.PckgVersionInfo O_pkg_vers_info, O_pkg_vers_info_prev;
+		MutableParsingState OM_parsing_state;
+		
+		int I_line_nbr_f1, I_line_nbr_of_pckg_start_f1;
+		StringBuilder SB_description_long;
+		String S_msg_1, S_msg_2, S_line_input,
+		S_pckg_name, S_description_short, S_description_long, S_description_long_part, S_closing_quote, 
+		S_category, AS_category[], S_requires, AS_requires[];
+		ReadLinePolicy E_read_line_policy;
+		
+		O_retval_setup_ini_contents = new SetupIniContents(PI_O_buff_reader);
+		SB_description_long = new StringBuilder();
+		E_parsing_state = ParsingState.Init;
+		
+		S_pckg_name          = null;
+		S_description_short  = null;
+		S_description_long   = null;
+		AS_category          = null;
+		AS_requires          = null;
+		O_pkg_vers_info      = null; 
+		O_pkg_vers_info_prev = null;
+		
+		I_line_nbr_f1               = 0;
+		I_line_nbr_of_pckg_start_f1 = 0;
+		S_line_input                = null;
+		E_read_line_policy          = ReadLinePolicy.ReadNext;
+		
+		LOOP_INPUT_LINES: while (E_parsing_state.ordinal() < I_parsing_state_finished) {
+			I_line_nbr_f1 = PI_O_buff_reader.I_curr_line_nbr;
+			switch (E_read_line_policy) {
+				case ReadNext: 
+					try {
+						S_line_input = PI_O_buff_reader.FS_readLine();
+						I_line_nbr_f1 = PI_O_buff_reader.I_curr_line_nbr;
+					} catch (RuntimeException PI_E_rt) {
+						S_msg_1 = "Error reading input file at line: " + I_line_nbr_f1;
+						E_rt = new RuntimeException(S_msg_1, PI_E_rt);
+						throw E_rt;
+					    }
+					break;
+					case ReRead: 
+						try {
+							S_line_input = PI_O_buff_reader.FS_re_read_line();
+							I_line_nbr_f1 = PI_O_buff_reader.I_curr_line_nbr;
+						} catch (RuntimeException PI_E_rt) {
+							S_msg_1 = "Error re-reading input file at line: " + I_line_nbr_f1;
+							E_rt = new RuntimeException(S_msg_1, PI_E_rt);
+							throw E_rt;
+						    }
+					case SkipRead:
+						E_read_line_policy = ReadLinePolicy.ReadNext;
+						break;
+			}
+			if (S_line_input == null) {
+				E_parsing_state = ParsingState.Finished;
+				   O_retval_setup_ini_contents.FV_add(
+						   S_pckg_name,
+							S_description_short,
+							S_description_long,
+							AS_category,
+							AS_requires,
+							O_pkg_vers_info, 
+							O_pkg_vers_info_prev,
+							I_line_nbr_of_pckg_start_f1);   
+				   break LOOP_INPUT_LINES;
+			    }
+			if (StringUtils.isBlank(S_line_input)) {
+				continue LOOP_INPUT_LINES;
+			    }
+			O_grp_match_res = RegexpUtils.FO_match(S_line_input, P_comment_line);
+			if (O_grp_match_res.I_array_size_f1 >= 1) {
+				continue LOOP_INPUT_LINES;
+			    }
+			if ((E_parsing_state == ParsingState.InterPgk) || (E_parsing_state == ParsingState.Init)) {
+				O_grp_match_res = RegexpUtils.FO_match(S_line_input, P_pkg_hdr);
+				if (O_grp_match_res.I_map_size_f1 >= 1) {
+				   if (E_parsing_state == ParsingState.InterPgk) {
+					   O_retval_setup_ini_contents.FV_add(
+						   S_pckg_name,
+						   S_description_short,
+						   S_description_long,
+						   AS_category,
+						   AS_requires,
+						   O_pkg_vers_info, 
+						   O_pkg_vers_info_prev,
+						   I_line_nbr_of_pckg_start_f1);   
+					   
+					   S_description_short  = null;
+					   S_description_long   = null;
+					   AS_category          = null;
+					   AS_requires          = null;
+					   O_pkg_vers_info      = null; 
+					   O_pkg_vers_info_prev = null;
+				       }
+					
+					S_pckg_name = O_grp_match_res.HS_named_groups.get(PKG_NAME).S_val;
+					I_line_nbr_of_pckg_start_f1 = I_line_nbr_f1;
+					E_parsing_state = ParsingState.PkgName;
+//					S_msg_1 = "Package-name: " + S_pkg_name;
+//					System.out.println(S_msg_1);
+					
+			        }
+			     }
+			else if (E_parsing_state == ParsingState.PkgName) {
+				O_grp_match_res = RegexpUtils.FO_match(S_line_input, P_sdesc);
+				if (O_grp_match_res.I_map_size_f1 >= 1) {
+					S_description_short = O_grp_match_res.HS_named_groups.get(DESCRIPTION_SHORT).S_val;
+					E_parsing_state = ParsingState.Sdesc;
+//					S_msg_1 = "Short-Descrition: " + S_description_short;
+//					System.out.println(S_msg_1);
+			        }
+		     }
+			else if (E_parsing_state == ParsingState.Sdesc) {
+				O_grp_match_res = RegexpUtils.FO_match(S_line_input, P_ldesc);
+				SB_description_long = new StringBuilder();
+				if (O_grp_match_res.I_map_size_f1 >= 2) {
+				   S_description_long = O_grp_match_res.HS_named_groups.get(DESCRIPTION_LONG).S_val;	
+			       S_closing_quote = O_grp_match_res.HS_named_groups.get(CLOSING_QUOTE).S_val;
+			       if (StringUtils.isEmpty(S_closing_quote)) {  // line containing long description will be continued
+			    	   E_parsing_state = ParsingState.LdescCont;
+			    	   SB_description_long = new StringBuilder(S_description_long);
+			           }
+			       else {
+			    	   E_parsing_state = ParsingState.Ldesc; // line containing long description complete
+			           } 
+				   S_description_long_part = O_grp_match_res.HS_named_groups.get(DESCRIPTION_LONG).S_val;
+			      // System.out.println(ParsingState.Sdesc.name());
+				}
+			}
+			else if (E_parsing_state == ParsingState.LdescCont)  {
+				O_grp_match_res = RegexpUtils.FO_match(S_line_input, P_ldesc_cont);
+				if (O_grp_match_res.I_map_size_f1 >= 2) {
+					S_description_long_part =  O_grp_match_res.HS_named_groups.get(DESCRIPTION_LONG).S_val;
+					
+					if (StringUtils.isNotBlank(S_description_long_part)) {
+						SB_description_long.append(" ");
+					   }
+					SB_description_long.append(S_description_long_part);
+					S_closing_quote = O_grp_match_res.HS_named_groups.get(CLOSING_QUOTE).S_val;
+					if (StringUtils.isNotEmpty(S_closing_quote)) {  // lines containing long description complete
+					   E_parsing_state = ParsingState.Ldesc;
+					   S_description_long = SB_description_long.toString(); 
+					 }
+				}
+			}
+			else if (E_parsing_state == ParsingState.Ldesc) {
+				O_grp_match_res = RegexpUtils.FO_match(S_line_input, P_category);
+				if (O_grp_match_res.I_map_size_f1 >= 1) {
+				   S_category = O_grp_match_res.HS_named_groups.get(CATEGORY).S_val;
+				   AS_category = S_category.split("\\s+");
+				   E_parsing_state = ParsingState.Category;
+				   }
+			    }
+			else if (E_parsing_state == ParsingState.Category)  {
+				O_grp_match_res = RegexpUtils.FO_match(S_line_input, P_requires);
+				if (O_grp_match_res.I_map_size_f1 >= 1) {
+				   S_requires = O_grp_match_res.HS_named_groups.get(REQUIRES).S_val;
+				   AS_requires = S_requires.split("\\s+");
+				   E_parsing_state = ParsingState.Requires;
+				   }
+			    }
+			else if (E_parsing_state == ParsingState.Requires)  {
+				OM_parsing_state = new MutableParsingState(E_parsing_state);
+				O_pkg_vers_info = FO_parse_pck_info(PI_O_buff_reader, OM_parsing_state);
+				if (O_pkg_vers_info == null) {
+					S_msg_1 =  "Error when parsing \'" + E_parsing_state.toString() + "\'";
+					E_np = new NullPointerException(S_msg_1);
+					S_msg_2 = "Error at line: " + I_line_nbr_f1 + "\n" +
+				              "last confirmed sucessful parsing state: " + OM_parsing_state.getValue().toString();
+					E_ass = new AssertionError(S_msg_2, E_np);
+					throw E_ass;
+				   }
+				else {
+				   E_parsing_state = OM_parsing_state.getValue();
+				   E_read_line_policy = ReadLinePolicy.ReRead;
+				   }
+			    }
+			else if ((E_parsing_state == ParsingState.Source) || (E_parsing_state == ParsingState.Install)) {
+				if (StringUtils.startsWith(S_line_input, PREV_MARKER)) {
+				   E_parsing_state = ParsingState.Prev;
+				   }
+				else {
+					E_parsing_state = ParsingState.InterPgk;  // no [prev] line_found
+					O_pkg_vers_info_prev = null;
+					E_read_line_policy   = ReadLinePolicy.SkipRead;
+				    }
+		        }
+			else if (E_parsing_state == ParsingState.Prev) {
+				OM_parsing_state = new MutableParsingState(ParsingState.Requires /*,  I_prev_offset */);
+				O_pkg_vers_info_prev = FO_parse_pck_info(PI_O_buff_reader, OM_parsing_state);
+				if (O_pkg_vers_info_prev == null) {
+					S_msg_1 =  "Error when parsing \'" + E_parsing_state.toString() + "\'";
+					E_np = new NullPointerException(S_msg_1);
+					S_msg_2 = "Error at line: " + I_line_nbr_f1 + "\n" +
+				              "last confirmed sucessful parsing state: " + OM_parsing_state.getValue().toString();
+					E_ass = new AssertionError(S_msg_2, E_np);
+					throw E_ass;
+				   }
+				else {
+				   E_parsing_state = OM_parsing_state.getValue();
+				   if ((E_parsing_state == ParsingState.Install) || (E_parsing_state == ParsingState.Source)) {
+					   E_parsing_state = ParsingState.InterPgk;
+				       }
+				   }
+			    }
+			if (E_parsing_state == ParsingState.Finished) {
+			   O_retval_setup_ini_contents.FV_add(
+					   S_pckg_name,
+					   S_description_short,
+					   S_description_long,
+					   AS_category,
+					   AS_requires,
+					   O_pkg_vers_info, 
+					   O_pkg_vers_info_prev,
+					   I_line_nbr_of_pckg_start_f1); 
+			   
+			   break LOOP_INPUT_LINES;	
+			}
+		}
+        return O_retval_setup_ini_contents;	    
+     }
+}
+
---
+++ .gitignore/src/main/java/com/github/michaelederaut/cygwinparser/ParseCygwin.java
@@ -0,0 +1,208 @@
+package com.github.michaelederaut.cygwinparser;
+
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.UnsupportedEncodingException;
+import java.nio.channels.FileChannel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.lang3.mutable.MutableInt;
+
+import com.github.michaelederaut.basics.LineNbrRandomAccessFile;
+import com.github.michaelederaut.basics.RegexpUtils;
+
+import java.net.URLEncoder;
+
+import regexodus.Pattern;
+
+public class ParseCygwin {
+
+public static final String S_dnr_x86_64         = "x86_64";
+public static final String S_dnr_categories_log = "_category_logs";
+public static final String S_bn_setup_ini       = "setup.ini";
+// public static final String S_bn_categories_log = "categories.log";
+public static final String S_bnt_categories_log = "categories";
+
+public static final String S_pna_setup_rc      = "C:\\cygwin64\\etc\\setup\\setup.rc";
+public static final String S_TIME_STAMP        = "yyyy-MM-dd_HH_mm_ss";
+public static final SimpleDateFormat O_timestamp_template = new SimpleDateFormat(S_TIME_STAMP);
+
+private static String FS_get_site_root(
+		final String PI_S_dna_cygw_repository_root,
+		final String PI_S_dnr_site_root) {
+	
+	IOException      E_io;
+	RuntimeException E_rt;
+	File             F_dl_site_root;
+	String S_msg_1, S_msg_2, S_rep_url_encoded;
+	
+	String S_retval_dl_site_root = null;
+	
+	   S_retval_dl_site_root =  PI_S_dna_cygw_repository_root + "\\" + PI_S_dnr_site_root;
+	   F_dl_site_root = new File(S_retval_dl_site_root);
+	   if (!F_dl_site_root.isDirectory()) {
+			S_msg_1 = "Unable to loacte folder: \"" + S_retval_dl_site_root + "\"";
+			E_io = new IOException(S_msg_1);
+			S_msg_2 = "Unable to locate file for download-site: \"" + PI_S_dnr_site_root + "\" with \'" + SetupConfigContents.S_encoding + "\'";
+			E_rt = new RuntimeException(S_msg_2, E_io);
+			throw E_rt;
+		    }
+
+	return S_retval_dl_site_root;
+    }
+
+
+    public static void main( String[] PI_as_args ) {
+   
+    	IOException      E_io;
+    	RuntimeException E_rt;
+       
+    	Date              O_timemstamp_current;
+    	FileWriter        O_fwrtr_categories_log;
+    	FileOutputStream  O_fs_out_categories_xslx;
+    	
+    	MutableInt       O_MI_nbr_lines_written;
+    	
+        LineNbrRandomAccessFile O_rdr_setup_ini, O_buff_wrtr_categories_log;
+    	SetupIniContents O_setup_ini_contents;
+    	SetupConfigContents O_config_contents;
+    	SetupConfigContents.Site O_site_current;
+    	ArchiveChecker   O_archive_checker;
+    	
+    	File F_dna_86_64, F_dna_category_logs, F_pna_setup_ini, F_pna_categories_log,  F_pna_categories_xslx;
+    	String S_site_root, S_dnr_site_root, S_dna_cygw_repository_root, S_dna_x86_64, S_dna_catgegory_logs, S_pna_setup_ini, 
+    	S_bntd_categories, S_bn_categories_log, S_bn_categories_xlsx, S_pna_categories_log, S_pna_categories_xlsx, S_time_stamp, S_msg_1, S_msg_2;
+    	
+    	long L_timestamp_current;
+    	int I_nbr_lines_written_f1, I_flags_dummy, I_idx_mirror_f0;
+    	boolean B_dir_created;
+    	
+    	O_config_contents = SetupConfigParser.FO_parse(S_pna_setup_rc);
+    	I_idx_mirror_f0 = O_config_contents.I_idx_last_mirror_f0;
+    	O_site_current = O_config_contents.AO_mirror_list.get(I_idx_mirror_f0);
+    	S_dnr_site_root = O_site_current.S_dnr_url_encoded;
+    	S_dna_cygw_repository_root = O_config_contents.S_pna_last_cache;
+    	
+    	S_site_root = FS_get_site_root(S_dna_cygw_repository_root, S_dnr_site_root);
+    	System.out.println("Site-root: " + S_site_root);
+ 	
+    	S_dna_x86_64         = S_site_root + "\\" + S_dnr_x86_64;
+    	S_dna_catgegory_logs = S_site_root + "\\" + S_dnr_categories_log; 
+    	F_dna_86_64          = new File(S_dna_x86_64);
+    	F_dna_category_logs =  new File(S_dna_catgegory_logs);
+    	
+    	B_dir_created = false;
+    	
+    	try {
+		   if (F_dna_category_logs.isDirectory()) {
+			   B_dir_created = true; 
+		       }
+		   else {
+			  B_dir_created = F_dna_category_logs.mkdir();
+			    }
+		} catch (SecurityException PI_E_sec) {
+			S_msg_1 = "Error creating Folder: \"" + S_dna_catgegory_logs + "\"";
+			E_rt = new RuntimeException(S_msg_1, PI_E_sec);
+			throw E_rt;
+		}
+    	if (!B_dir_created)	{
+    		S_msg_1 = "Unable to create folder: \"" + S_dna_catgegory_logs + "\"";
+    		E_io = new IOException(S_msg_1);
+    		S_msg_2 = "Unable to create folder containing the category logs.";
+    		E_rt = new RuntimeException(S_msg_2, E_io);
+    		throw E_rt;
+    	}
+    	
+    	if (!F_dna_86_64.isDirectory()) {
+    		S_msg_1 = "Unable to locate folder: \'" + S_dna_x86_64 + "\"";
+    		E_io = new IOException(S_msg_1);
+    		S_msg_2 = "Unable to locate sub-folder: \'" + S_dnr_x86_64 + "\"";
+    		E_rt = new RuntimeException(S_msg_2, E_io);
+    		throw E_rt;
+    	    }
+    	
+    	S_pna_setup_ini = S_dna_x86_64 + "\\" + S_bn_setup_ini;
+    	F_pna_setup_ini = new File(S_pna_setup_ini);
+   
+    	S_msg_1         = null;
+    	if (!F_pna_setup_ini.exists()) {
+    		S_msg_1 = "Unable to locate file: \'" + S_pna_setup_ini + "\"";
+    	    }
+    	else if (!F_pna_setup_ini.isFile()) {
+    		S_msg_1 = "Object \'" + S_pna_setup_ini + "\" is not a regular file"; 
+    	    }
+    	else if (!F_pna_setup_ini.canRead()) {
+    		S_msg_1 = "File \'" + S_pna_setup_ini + "\" is not readable"; 
+    	    }
+    	
+   	    if (S_msg_1 != null) {
+    		E_io = new IOException(S_msg_1);
+    	    S_msg_2 = "Unable to find readable file: \'" + S_bn_setup_ini + "\'";
+    	    E_rt = new RuntimeException(S_msg_2, E_io);
+    	    throw E_rt;
+    	    }
+    	
+    	try {
+			O_rdr_setup_ini = new LineNbrRandomAccessFile(F_pna_setup_ini, LineNbrRandomAccessFile.READ_ONLY);
+		} catch (FileNotFoundException PI_E_fnf) {
+			S_msg_1 = "Unable to instantiate a file-object of type \'" + LineNbrRandomAccessFile.class.getName() + "\' " + 
+		              "from path \"" + F_pna_setup_ini.getPath() + "\"  with flag(s): \'" + LineNbrRandomAccessFile.READ_ONLY + "\'";
+			E_rt = new RuntimeException(S_msg_1, PI_E_fnf);
+			throw E_rt;
+		    }
+    	
+    	O_setup_ini_contents = IniFileParser.FO_parse(O_rdr_setup_ini);
+    	O_rdr_setup_ini.FV_close();
+    	O_archive_checker = new ArchiveChecker(S_dna_cygw_repository_root);
+    	O_archive_checker.FI_check_pckgs(S_dnr_site_root, O_setup_ini_contents);
+    	
+    	L_timestamp_current = System.currentTimeMillis();
+    	O_timemstamp_current = new Date(L_timestamp_current);		
+    	S_time_stamp = O_timestamp_template.format(O_timemstamp_current);
+    	S_bntd_categories = S_bnt_categories_log + "." + 
+    	                      O_site_current.E_protocol.getName().toLowerCase()  + "_" + 
+    			              O_site_current.S_site + "_" + 
+    			              S_time_stamp + ".";
+    	S_bn_categories_log =   S_bntd_categories + "log";
+    	S_pna_categories_log = S_dna_catgegory_logs   + "\\" + S_bn_categories_log;
+    	F_pna_categories_log = new File(S_pna_categories_log);
+    	
+    	S_bn_categories_xlsx =  S_bntd_categories + "xlsx";
+    	S_pna_categories_xlsx =  S_dna_catgegory_logs + "\\" + S_bn_categories_xlsx;
+    	F_pna_categories_xslx = new File(S_pna_categories_xlsx); 
+    	
+    	try {
+			O_fs_out_categories_xslx = new FileOutputStream(F_pna_categories_xslx, false);
+		} catch (FileNotFoundException PI_E_fnf) {
+			S_msg_1 = "Unable to create file: \"" + S_pna_categories_xlsx + "\""; 
+			E_rt = new RuntimeException(S_msg_1, PI_E_fnf);
+			throw E_rt;
+		}
+
+    	O_MI_nbr_lines_written = new MutableInt(0);
+    	O_archive_checker.FV_eval_categories(
+    			O_fs_out_categories_xslx,
+    			S_dnr_site_root,
+    			O_setup_ini_contents,
+    			O_MI_nbr_lines_written);
+
+    	
+    	I_nbr_lines_written_f1 = O_MI_nbr_lines_written.getValue();
+    	S_msg_1 = I_nbr_lines_written_f1 + " rows written to worksheet: \"" + F_pna_categories_xslx.getAbsolutePath() + "\".";
+    	System.out.println("\n" + S_msg_1);
+
+    	try {
+			O_fs_out_categories_xslx.close();
+		} catch (IOException PI_E_io) {
+			S_msg_1 = "Unable to close " + O_fs_out_categories_xslx.getClass().getSimpleName() + " \"" + F_pna_categories_xslx.getPath() + "\"";
+			E_rt = new RuntimeException(S_msg_1, PI_E_io);
+			throw E_rt;
+		}
+    }
+}
